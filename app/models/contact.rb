e22ce2a2badb35a7b6e9fe94270f7e0ba5f37a8e
Jakob Sitko <jakobsitko.js@googlemail.com>
2017-01-21 21:00:23 +0000

    Added a destriction for Contact - Rules/validates

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,9 @@
 class Contact < ActiveRecord::Base
     #dont need to write out the values of a comment because rails checks
     #the data base :)
-    #this is the blueprint! IDEAL CONTACT
+    #this is the blueprint! IDEAL CONTACT(WAS IST EIN RICHTIGER CONTACT? DAS HIER:)
+    #Rules!:
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :comments, presence: true
 end
