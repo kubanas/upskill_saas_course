108cf49838ae7596c8e037d0af53bbe08ff8e60a
Jakob Sitko <jakobsitko.js@googlemail.com>
2017-01-21 20:14:21 +0000

    Added CreateAction to save to DB

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,5 @@
 class Contact < ActiveRecord::Base
-    
-    
-    
-    
-    
+    #dont need to write out the values of a comment because rails checks
+    #the data base :)
+    #this is the blueprint! IDEAL CONTACT
 end
