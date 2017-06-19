687109abd7a84add361b08f0543aafd3d944d962
Jakob Sitko <jakobsitko.js@googlemail.com>
2017-06-19 15:52:31 +0000

    Modified the signup forms

---
+++ test.rb
@@ -0,0 +1,112 @@
+# # # # # # m=2
+# # # # # # n=6
+# # # # # # i=n-m
+# # # # # # k=m+n
+
+# # # # # # sum=(i+1)*(k/2)
+ 
+# # # # # # puts sum
+
+# # # # # ###### Summereihe
+# # # # # m=1
+# # # # # n=100
+# # # # # i=1
+# # # # # d=n-m
+
+# # # # # while i<n-2
+
+# # # # #     sum=(d+1)*(m+n)/2
+# # # # #     i=i+1
+    
+# # # # # end
+
+# # # # # puts sum
+
+
+# # # # preis_a=500
+# # # # preis_b=20
+
+# # # # summe=preis_a+preis_b
+# # # # puts summe
+
+
+# # # # if preis_a < preis_b
+# # # #   preis=summe-preis_a 
+        
+# # # # else
+# # # #     preis=summe-preis_b
+    
+# # # # end
+
+# # # # puts preis
+
+
+# # # # summe=-51
+# # # # if summe<0
+# # # #   puts"Achtung, es ist ein Fehler aufgetreten"
+    
+# # # #   else
+# # # #     puts summe
+       
+# # # # end
+
+
+# # # # begin
+# # # #     preis=40
+# # # #     summe=summe+preis
+    
+# # # # end while summe<300 
+    
+
+
+# # # # puts summe
+
+
+# # # erg=0
+# # # i=0
+# # # n=5
+
+# # # while i<=n
+# # #     erg=erg+i
+# # #     i=i+1
+# # # end
+
+# # # puts erg
+
+
+# # begin cpu_last=gets.to_i
+
+ 
+    
+# #     if cpu_last<60
+# #         puts "green light"
+# #     end
+
+# #     if cpu_last >= 60 && cpu_last <= 80
+# #         puts "yellow light"
+# #     end
+    
+# #     if cpu_last>80
+# #         puts "redlight"
+# #     end
+
+
+# # end while cpu_last < 100
+
+# # # puts "Prozesse werden jetzt beendet!"
+
+
+# Kalorien=0
+# Pralinen=4
+# i=0
+# begin
+#     Pralinen=Pralinen-1
+#     Kalorien=Kalorien+200
+#     i=i+1
+
+# end while Pralinen>0 && Kalorien<2000
+
+
+
+# puts Kalorien
+# puts i
