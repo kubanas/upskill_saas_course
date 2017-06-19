8c9d427fdcd86e5226b3512cb5f2c4c09466ec2c
Jakob Sitko <jakobsitko.js@googlemail.com>
2017-06-19 16:36:28 +0000

    Added Stripe Customer Token to Users DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170204140956) do
+ActiveRecord::Schema.define(version: 20170619163357) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20170204140956) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
- 
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170204140956) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "plan_id"
+    t.string   "stripe_customer_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
